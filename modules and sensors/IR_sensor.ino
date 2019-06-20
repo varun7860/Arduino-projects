9daa7206096689dc80555773f9c2f5afdc602ec9
Varun walimbe <51876661+varun7860@users.noreply.github.com>
2019-06-20 20:51:01 +0530

    Add files via upload

---
+++ modules and sensors/IR_sensor.ino
@@ -0,0 +1,22 @@
+const int ProxSensor=3; //Declaring where the Out pin from the sensor is wired
+
+void setup() 
+{                
+  pinMode(13, OUTPUT);   // setting the pin modes, the "13" stands for the internal Arduino uno internal LED  
+  pinMode(ProxSensor,INPUT); // then we have the out pin from the module
+}
+
+void loop() 
+{
+  
+  if(digitalRead(ProxSensor)==HIGH)      //Check the sensor output if it's high
+             {
+               digitalWrite(13, LOW);   // Turn the  LED on (Yes by writing LOW)
+             }
+  else
+            {
+              digitalWrite(13, HIGH);    // Turn the LED OFF if there's no signal on the ProxSensor
+             }
+  delay(100);             
+
+}
---
+++ modules and sensors/bluetooth_module.ino
@@ -0,0 +1,36 @@
+ char data =0;
+ 
+void setup()
+{
+ pinMode(12,OUTPUT);
+ Serial.begin(9600);
+}
+
+void loop() 
+{
+  if(Serial.available()>0)
+  {
+  data = Serial.read();
+  Serial.print(data);
+  digitalWrite(12, HIGH);
+}
+ if (data == '0')
+ {
+  digitalWrite(12, LOW);
+    {
+      Serial.println("12 :off");
+    
+    }
+ }
+  else if(data == '1')
+  {
+    digitalWrite(12, HIGH);
+    
+    {
+      Serial.println("12: on");
+     
+    }
+  }
+}
+  
+
