9a478fd043366460e950aa155c4aa6045468291b
Varun walimbe <51876661+varun7860@users.noreply.github.com>
2019-06-20 21:07:43 +0530

    Update displaying_on_lcd.ino

--- modules and sensors/displaying_on_lcd.ino
+++ modules and sensors/displaying_on_lcd.ino
@@ -1,18 +1,18 @@
 #include <LiquidCrystal.h> 
-int Contrast=75;
+int Contrast=75;  // 75 is the contrast we are setting for LCD . You can set any value you want. Dont set it too high or low
  LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  
 
  void setup()
  {
     analogWrite(6,Contrast);
-     lcd.begin(16, 2);
+     lcd.begin(16, 2); // we are using 16*2 LCD
   } 
      void loop()
  { 
-     lcd.setCursor(0, 0);
+     lcd.setCursor(0, 0); //first line of LCD
      lcd.print("Hello customer");
    
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0, 1);// second line of LCD
      lcd.print("get coupons here");
 
  }
