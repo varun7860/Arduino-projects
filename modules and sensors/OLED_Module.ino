e996fba0819363a9ff4356e6fd050c87335f5c27
Varun walimbe <51876661+varun7860@users.noreply.github.com>
2020-05-13 22:58:44 +0530

    Create OLED_Module.ino

---
+++ modules and sensors/OLED_Module.ino
@@ -0,0 +1,50 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSerif9pt7b.h>
+
+#define screen_width 128 //display width in pixels
+#define screen_height 64 // 
+
+//Instance 
+Adafruit_SSD1306 display(screen_width, screen_height, &Wire, -1);
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+}
+  delay(2000);
+
+  display.setFont(&FreeSerif9pt7b);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,10);
+  // Display static text
+  display.println("Hello, Varun!");
+  display.display(); 
+  delay(100);
+  }
+void loop() 
+{
+   /*display.startscrollright(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrollleft(0x00, 0x0F);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  display.startscrolldiagright(0x00, 0x07);
+  delay(2000);
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(2000);
+  display.stopscroll();
+  delay(1000);
+  */
+}
