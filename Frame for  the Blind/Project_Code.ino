35da857e4ef07c2be53aa2c57da9deb343b6d763
Varun walimbe <51876661+varun7860@users.noreply.github.com>
2021-02-04 22:16:11 +0530

    Update Project_Code.ino

--- Frame for  the Blind/Project_Code.ino
+++ Frame for  the Blind/Project_Code.ino
@@ -1,3 +1,7 @@
+/*This is the final code of the project: Obstacle Detection for the Blind.
+If you have any doubts refer the project description.md file of this
+Project.*/
+
 int Trigpin = 5; // Any digital I/O Pin can be used here
 int Echopin = 4; // Any digital I/O Pin can be used here
 const int buzzer= 10; // Any digital I/O Pin can be used here
